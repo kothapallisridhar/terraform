92b0b9bf1ca74b94d84652181b446f54735c9058
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-05-28 12:33:38 +0530

    create ec2 instance

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -22,3 +22,13 @@ resource "aws_security_group" "allow_ssh" {
     CreatedBy = "Sridhar"
   }
 }
+
+resource "aws_instance" "db" {
+  ami = "ami-090252cbe067a9e58"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "db"
+  }  
+}
