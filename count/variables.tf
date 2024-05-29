5f612b8306f8b02b6f9ace4c35f87862d0c52997
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-05-29 13:57:35 +0530

    variables

---
+++ count/variables.tf
@@ -0,0 +1,50 @@
+#1. command line
+#2. tfvars
+#3. ENV variable
+#4. variable default value
+
+variable "instance_names" {
+  type = list
+  default = ["DB", "Backend", "Frontend"]
+}
+
+variable "image_id" {
+  type        = string #optional
+  default     = "ami-090252cbe067a9e58" #optional
+  description = "RHEL-9 AMI ID" #optional
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+    type = string
+}
+
+variable "tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "DB"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
