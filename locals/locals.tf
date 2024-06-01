0fca20d81f06a1755ac5790c164357166fa69e09
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2024-06-01 11:12:51 +0530

    locals

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  ami_id = 
-  sg_id = 
+  ami_id = "ami-090252cbe067a9e58"
+  sg_id = "sg-07a2609d1c0f0371e"
   instance_type = var.instance_name == "db" ? "t3.micro" : "t2.micro"
   tags = {
     Name = "locals"
